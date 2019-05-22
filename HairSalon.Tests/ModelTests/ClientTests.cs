baf7385e75ddb9cfb46fa85e159b83ae37e30983
Neil Bateman <neilbatman@gmail.com>
2019-05-21 20:13:16 -0700

    changed port numbers

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -16,7 +16,7 @@ namespace HairSalon.Tests
 
     public ClientTest()
     {
-      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3308;database=neil_bateman_test;";
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=neil_bateman_test;";
     }
 
     [TestMethod]
